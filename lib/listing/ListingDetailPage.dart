822bb7e745fccef5c6009d1ab50bfa2a578ffd42
DevBeastTeam <devbeast143@gmail.com>
2025-08-09 07:56:14 -0700

    listing

--- lib/listing/ListingDetailPage.dart
+++ lib/listing/ListingDetailPage.dart
@@ -15,46 +15,79 @@ class ListingDetailPage extends StatelessWidget {
       appBar: AppBar(title: const Text("listing Details")),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CacheImageWidget(
-              width: 300,
-              height: 200,
-              isCircle: false,
-              radius: 0,
-              url:
-                  Config.imgUrl + fullData['images'][0] ??
-                  imgLinks.profileImage,
-            ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CacheImageWidget(
+                width: 300,
+                height: 150,
+                isCircle: false,
+                radius: 0,
+                url:
+                    Config.imgUrl + fullData['images'][0] ??
+                    imgLinks.profileImage,
+              ),
 
-            Text(
-              "${fullData['title'] ?? 'Title.......'}",
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
+              Text(
+                "${fullData['title'] ?? 'Title.......'}",
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
 
-            const SizedBox(height: 8),
-            // Text(
-            //   "${fullData['desc'] ?? 'Description.......'}",
-            //   style: const TextStyle(fontSize: 13, fontWeight: FontWeight.w400),
-            // ),
-            HtmlWidget(fullData['description'] ?? 'Description.......'),
+              const SizedBox(height: 8),
+              // Text(
+              //   "${fullData['desc'] ?? 'Description.......'}",
+              //   style: const TextStyle(fontSize: 13, fontWeight: FontWeight.w400),
+              // ),
+              HtmlWidget(fullData['description'] ?? 'Description.......'),
 
-            Divider(),
-            ListTile(title: Text("From User")),
-            // ListTile(
-            //   leading: CacheImageWidget(
-            //     width: 50,
-            //     height: 50,
-            //     isCircle: true,
-            //     radius: 200,
-            //     url:
-            //         Config.imgUrl + fullData['images'] ?? imgLinks.profileImage,
-            //   ),
-            // title: Text(fullData['fromuid']['name'] ?? 'Unknown'),
-            // subtitle: Text(fullData['fromuid']['email'] ?? 'Unknown'),
-            // ),
-          ],
+              Divider(),
+
+              ListTile(
+                title: Text("dailyrate: ${fullData['dailyrate'] ?? '0'}"),
+              ),
+              Divider(),
+              ListTile(
+                title: Text("weeklyrate: ${fullData['weeklyrate'] ?? '0'}"),
+              ),
+              Divider(),
+              ListTile(
+                title: Text(" monthlyrate: ${fullData['monthlyrate'] ?? '0'}"),
+              ),
+              Divider(),
+              ListTile(
+                title: Text("created_at: ${fullData['created_at'] ?? '0'}"),
+              ),
+              Divider(),
+              ListTile(
+                title: Text(" updated_at: ${fullData[' updated_at'] ?? '0'}"),
+              ),
+              Divider(),
+              ListTile(
+                title: Text(
+                  " availabilityDays: ${fullData['availabilityDays'] ?? '0'}",
+                ),
+              ),
+              Divider(),
+
+              ListTile(title: Text("From User")),
+              ListTile(
+                leading: CacheImageWidget(
+                  width: 50,
+                  height: 50,
+                  isCircle: true,
+                  radius: 200,
+                  url:
+                      Config.imgUrl + fullData['images'][0] ??
+                      imgLinks.profileImage,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
